d1ebcef73ee36941ff2e1f0f2190577b060be43f
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2025-07-18 20:31:26 +0600

    Fix non-lazy computation of ternary operators

--- src/com/github/inc0grepoz/lix4j/unit/expression/OperatorTernary.java
+++ src/com/github/inc0grepoz/lix4j/unit/expression/OperatorTernary.java
@@ -15,10 +15,10 @@ public class OperatorTernary extends Operator
     @Override
     public Object evaluate(ExecutionContext ctx, Accessor[] operands)
     {
-        boolean condition = PrimitiveTester.isDefaultValue(operands[0].linkedAccess(ctx, null));
-        Object rv1 = operands[1].linkedAccess(ctx, null);
-        Object rv2 = operands[2].linkedAccess(ctx, null);
-        return condition ? rv2 : rv1;
+        // This is important to keep the computation below lazy
+        return PrimitiveTester.isDefaultValue(operands[0].linkedAccess(ctx, null))
+                ? operands[2].linkedAccess(ctx, null)
+                : operands[1].linkedAccess(ctx, null);
     }
 
 }
