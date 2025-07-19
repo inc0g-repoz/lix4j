63ddb8d5a5225ccfdf08bcc69ca13e4595280f17
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2025-07-19 22:35:20 +0600

    Add javadocs for enum Modifier

--- src/com/github/inc0grepoz/lix4j/unit/Modifier.java
+++ src/com/github/inc0grepoz/lix4j/unit/Modifier.java
@@ -1,12 +1,32 @@
 package com.github.inc0grepoz.lix4j.unit;
 
+/**
+ * Represents a certain type of a modifier applied to a variable.
+ * 
+ * @author inc0g-repoz
+ */
 public enum Modifier
 {
 
-    STATIC, VAR;
+    /**
+     * Indicates that a variable is static (doesn't require lookups
+     * in the varpool for accessing).
+     */
+    STATIC,
+    /**
+     * Indicates that a variable is fresh (was just created) where
+     * used (purely for clarity in the current version).
+     */
+    VAR;
 
     private final String keyword = name().toString().toLowerCase();
 
+    /**
+     * Returns the lowercase string used as the keyword for this
+     * modifier in code.
+     * 
+     * @return a {@code String} value
+     */
     public String getKeyword()
     {
         return keyword;
